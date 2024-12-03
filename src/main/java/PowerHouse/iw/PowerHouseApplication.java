4566287b7ad5a8a42023877a073a1aeb92484191
Álvaro Pedreño Rubio <78800668+apr142-ua@users.noreply.github.com>
2024-12-03 17:59:43 +0100

    Hola Mundo

---
+++ src/main/java/PowerHouse/iw/PowerHouseApplication.java
@@ -0,0 +1,13 @@
+package PowerHouse.iw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PowerHouse/iw/PowerHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package PowerHouse.iw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
